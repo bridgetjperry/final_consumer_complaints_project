63adb5004e10ade54dff0ea7bd733727b9733f69
Bridget Perry <bridgetperry@Bridgets-MacBook-Pro.local>
2022-05-06 10:46:28 -0400

    Commit about page

---
+++ About.R
@@ -0,0 +1,29 @@
+title: "About"
+author: "Bridget Perry"
+date: "5/5/2022"
+
+
+#A couple of weeks ago, I looked at the consumer complaints dataset and analyzed
+#the data. After going over that data, I decided that for this final project I
+#wanted to focus on consumer complaint submission mediums (the way in which the
+#consumers submitted their complaints) as well as the timeliness of the company
+#responses. I built two tables and two charts, and each one represents this information
+#in a different way.
+
+#The first table shows all of the ways in which complaints were submitted, which
+#includes email, fax, phone, postal mail, referral, and the web. I paired this
+#information with the timeliness of responses from the companies. Yes means that
+#the response was timely, and No means it was too slow. I wanted to see if the
+#medium through which customers submitted complaints had any impact on the
+#speed at which their issue was handled. Proportionally, phone calls seem to result
+#in the least satisfying response time.
+
+#For my second table, I looked at the response time in conjunction with the company
+#response to the consumer. I wanted to see if the company response to the consumer
+#complaint had any bearing on whether the response time was satisfactory or not.
+
+#For my visualizations, I made two charts that display one variable. The first
+#shows submission mediums, and the second shows the responsiveness to complaints.
+#I wanted to make this information easier to visualize and not cluttered. For example,
+#the second chart makes it rather clear that response times were overwhelmingly
+#good. The first shows that most submissions, by far, were made through the web.
